de7da7bddf712de0356129355e88c9c99834b863
Gavin Jin <gavinjin62@gmail.com>
2019-05-25 11:51:30 -0700

    Add single coin restock

--- __tests__/vending-machine.spec.js
+++ __tests__/vending-machine.spec.js
@@ -1,80 +1,59 @@
 const VendingMachine = require("../src/vending-machine");
-const data = require("../vendingMachinData.json");
+const inv = require("../vendingMachinData.json");
+const coins = require("../coins.json");
 
 describe("VendingMachine", () => {
   let vendingMachine;
 
   beforeEach(() => {
     vendingMachine = {};
-
-    vendingMachine.data = {
-      inventory: {
-        coke: {
-          id: 0,
-          price: 1,
-          quantity: 10
-        },
-        fanta: {
-          id: 1,
-          price: 2,
-          quantity: 10
-        },
-        sprite: {
-          id: 2,
-          price: 3,
-          quantity: 10
-        },
-        pepsi: {
-          id: 3,
-          price: 4,
-          quantity: 10
-        },
-        tea: {
-          id: 4,
-          price: 5,
-          quantity: 10
-        }
-      }
-    };
+    vendingMachine.subject = new VendingMachine(inv, coins);
   });
 
   describe("queryInventory", () => {
-    beforeEach(() => {
-      vendingMachine.subject = new VendingMachine(data);
-      // vendingMachine.inventoryData = vendingMachine.subject.inventory;
-    });
     describe("when asked for inventory", () => {
       beforeEach(() => {
         vendingMachine.result = vendingMachine.subject.showInventory();
       });
       it("should give the inventory available", () => {
         // expect(test.result.averageAge).toEqual(test.demographics.averageAge);
-        expect(vendingMachine.result).toEqual({
-          coke: {
-            id: 0,
-            price: 1,
+        expect(vendingMachine.result).toEqual(inv.inventory);
+      });
+    });
+  });
+  describe("queryMoney", () => {
+    describe("when asked for amount of money in the vending machine", () => {
+      beforeEach(() => {
+        vendingMachine.result = vendingMachine.subject.queryMoney();
+      });
+      it("should give the amount of money", () => {
+        // expect(test.result.averageAge).toEqual(test.demographics.averageAge);
+        expect(vendingMachine.result).toEqual(vendingMachine.subject.quantity);
+      });
+    });
+  });
+  describe("Restock single coin", () => {
+    describe("Restock singular coin, with valid input", () => {
+      beforeEach(() => {
+        vendingMachine.result = vendingMachine.subject.restockSingularCoin({
+          coin: "loonie",
+          quantity: 11
+        });
+      });
+      it("Should increment the quantity of specified coin, by specified amount", () => {
+        expect(vendingMachine.result).toEqual(21);
+      });
+    });
+    describe("restock single item", () => {
+      describe("when vending machine inventory is empty", () => {
+        beforeEach(() => {
+          vendingMachine.result = vendingMachine.subject.restockSingleItem({
+            item: "coke",
             quantity: 10
-          },
-          fanta: {
-            id: 1,
-            price: 2,
-            quantity: 10
-          },
-          sprite: {
-            id: 2,
-            price: 3,
-            quantity: 10
-          },
-          pepsi: {
-            id: 3,
-            price: 4,
-            quantity: 10
-          },
-          tea: {
-            id: 4,
-            price: 5,
-            quantity: 10
-          }
+          });
+        });
+        it("should increase coin by given amount", () => {
+          expect(vendingMachine.result).toEqual(20);
         });
       });
     });
--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,11 +1,29 @@
 class VendingMachine {
-  constructor(data) {
-    this.data = data;
+  constructor(inv, coins) {
+    this.inv = inv;
+    this.coins = coins;
   }
 
   showInventory() {
-    return this.data.inventory;
+    return this.inv.quantity;
+  }
+  queryMoney() {
+    return this.coins.quantity;
+  }
+  restockSingularCoin({ coin, quantity }) {
+    if (typeof coin !== "string" || typeof quantity !== "number") {
+      throw new Error();
+    } else {
+      return (this.coins[coin].quantity += quantity);
+    }
+  }
+  restockSingleItem({ item, quantity }) {
+    console.log("HELLO", { item, quantity });
+    if (typeof coin !== "string" || typeof quantity !== "number") {
+      throw new Error();
+    } else {
+      return (this.inv[item].quantity += quantity);
+    }
   }
 }
-
 module.exports = VendingMachine;
